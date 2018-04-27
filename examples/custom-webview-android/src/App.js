7753eaf136626483a2e83117718519f33b0fd909
Andrei Pfeiffer <andrei@upsidedown.ro>
2018-04-27 17:05:12 +0300

    Add ref usage in the example

--- examples/custom-webview-android/src/App.js
+++ examples/custom-webview-android/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 import CustomWebView from "react-native-webview-android-image-upload";
 
 export default class App extends Component {
+  inject = () => {
+    this.webview.injectJavaScript("alert('JavaScript is injected')");
+  };
+
+  reload = () => {
+    this.webview.reload();
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <CustomWebView
           style={styles.container}
+          getRef={e => (this.webview = e)}
+          injectedJavaScript={"alert('Custom webview loaded')"}
           source={{
             uri:
               "https://andreipfeiffer.github.io/react-native-webview-android-image-upload/index.html"
           }}
         />
+
+        <View style={styles.containerHorizontal}>
+          <Button title="Reload" onPress={this.reload} />
+          <Button title="Inject JS" onPress={this.inject} />
+        </View>
       </View>
     );
   }
@@ -21,5 +36,11 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  containerHorizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 10,
+    backgroundColor: "#eee"
   }
 });
